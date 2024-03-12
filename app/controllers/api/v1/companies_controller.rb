dbbb9a2bf71a4a48ce4dccf646609972a3c273d3
Alvaro Sanchez <hello@alvarosanchez.dev>
2024-03-12 13:06:55 -0600

    Company model can be filtered by name or industry

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,17 +1,28 @@
 class Api::V1::CompaniesController < ApplicationController
   def index
-    companies = Company.all
-    name_filter = company_params.dig(:query, :name)
-    companies = companies.filter_by_name(name_filter) if name_filter.present?
     companies.order(created_at: :desc)
     render json: companies.as_json(include: :deals)
+  end
+
+  def companies
+    @companies ||= Company.filter_query(**filter_hash)
+      .order(created_at: :desc)
   end
 
   private
 
   def company_params
     params.permit(
-      query: [:name]
+      query: [:industry, :name]
     )
+  end
+
+  def filter_hash
+    {
+      industry: "",
+      name: ""
+    }.merge(
+      company_params[:query] || {}
+    ).symbolize_keys
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,8 @@ class Company < ApplicationRecord
   has_many :deals
 
   scope :filter_by_name, ->(name) { where("LOWER(name) like ?", "%#{name.downcase}%") }
+  scope :filter_by_industry, ->(industry) { where("LOWER(industry) like ?", "%#{industry.downcase}%") }
+  scope :filter_query, ->(industry: "", name: "") {
+    filter_by_industry(industry).filter_by_name(name)
+  }
 end
